3f90f1574f2d6e97043eade5bf63561b003e2e1c
rizalPangestu <rizalpangestu4@gmail.com>
2019-10-14 15:03:13 +0700

    deleted latihan_cv_rizal_2 file

--- latihan_cv_rizal_2/template/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Script js -->
-
-</body>
-</html>
--- latihan_cv_rizal_2/template/head.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-    <title>Cv Rizal</title>
-    <link rel="stylesheet" href="./assets/css/main.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-</head>
-<body>
